b987f372b9b0abebdd7178078b753b75ce762eb9
Jaimedgp <jaime.diez.gp@gmail.com>
2022-10-25 09:20:30 +0200

    third try

--- scripts/git_branch_icon.sh
+++ scripts/git_branch_icon.sh
@@ -5,19 +5,17 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 print_icon() {
     # If we are on a folder not tracked by git, get out.
     # Otherwise, check for hide-info at global and local repository level
-    if ! __git_prompt_git rev-parse --git-dir &> /dev/null \
-        || [[ "$(__git_prompt_git config --get oh-my-zsh.hide-info 2>/dev/null)" == 1 ]]; then
-    return 0
-    fi
+    #if ! __git_prompt_git rev-parse --git-dir &> /dev/null \
+    #    || [[ "$(__git_prompt_git config --get oh-my-zsh.hide-info 2>/dev/null)" == 1 ]]; then
+    #return 0
+    #fi
 
-    local ref
-    ref=$(__git_prompt_git symbolic-ref --short HEAD 2> /dev/null) \
-    || ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) \
-    || return 0
-
+    #local ref
+    #ref=$(__git_prompt_git symbolic-ref --short HEAD 2> /dev/null) \
+    #|| ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) \
+    #|| return 0
 
     echo ""
-
 }
 
 main() {
